6523c634865d0e8ddf86065fb2f68f28513a4f77
Elaisy <47695753+Elaisy@users.noreply.github.com>
2019-04-25 14:52:34 +0200

    added more
    js doesn't work properly yet

--- js.js
+++ js.js
@@ -1,2 +1,17 @@
 function change() {
-    var image = document.getElementById()
+
+    var image = document.getElementById('imageCheckboxChecked');
+    if(image.src = "checkbox_unchecked.png") {
+        image.src = "checkbox_checked.png";
+    }else {image.src = "checkbox_unchecked.png"}
+       
+}
+
+function changeTwo() {
+
+    var image = document.getElementById('imageCheckboxCheckedtwo');
+    if(image.src = "checkbox_unchecked_two.png") {
+        image.src = "checkbox_checked_two.png";
+    }else {image.src = "checkbox_unchecked_two.png"}
+       
+}
